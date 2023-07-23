dd63a2668d991145b58040081a84586b1f9e759c
coldwater121112 <coldwater121112@gmail.com>
2023-07-24 00:55:00 +0800

    feat: add CRUD features & hbs

--- app.js
+++ app.js
@@ -11,7 +11,11 @@ require('./config/mongoose')
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
-app.use(methodOverride('_method'))
+
+// setting static files
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'));
 
 
 
@@ -60,6 +64,90 @@ app.get("/search", (req, res) => {
     })
 })
 
+
+app.get("/new", (req, res) => {
+  res.render("new")
+})
+
+// 新增餐廳
+app.post("/", (req, res) => {
+  Restaurant.create(req.body)
+    .then(() => res.redirect("/"))
+    .catch(error => {
+      console.log(error)
+      res.render(
+        'errorPage',
+        { error: error.message }
+      )
+    })
+})
+
+// 使用者可以瀏覽一家餐廳的詳細資訊
+app.get("/:restaurant_id", (req, res) => {
+  const { restaurant_id } = req.params
+  // console.log(restaurant_id)
+  // return Restaurant.findById(Object.values(restaurant_id))
+  return Restaurant.findById(restaurant_id)
+    .lean()
+    .then(restaurant => res.render('show', { restaurant }))
+    .catch(error => {
+      console.log(error)
+      res.render(
+        'errorPage',
+        { error: error.message }
+      )
+    })
+})
+
+// 使用者可以修改一家餐廳的資訊
+app.get("/:restaurant_id/edit", (req, res) => {
+  const { restaurant_id } = req.params
+  // console.log(restaurant_id)
+  return Restaurant.findById(restaurant_id)
+    .lean()
+    .then(restaurant => res.render('edit', { restaurant }))
+    .catch(error => {
+      console.log(error)
+      res.render(
+        'errorPage',
+        { error: error.message }
+      )
+    })
+})
+// post的寫法 Note:<form action="/restaurants/{{ restaurant._id }}" method="POST">
+// app.post("/restaurants/:restaurant_id", (req, res) => {
+//   const { restaurant_id } = req.params;
+//   return Restaurant.findByIdAndUpdate(restaurant_id, req.body)
+//     .then(() => res.redirect(`/restaurants/${restaurant_id}`))
+//     .catch(error => console.log(error));
+// });
+
+app.put("/:restaurant_id", (req, res) => {
+  const { restaurant_id } = req.params;
+  return Restaurant.findByIdAndUpdate(restaurant_id, req.body)
+    .then(() => res.redirect(`/restaurants/${restaurant_id}`))
+    .catch(error => {
+      console.log(error)
+      res.render(
+        'errorPage',
+        { error: error.message }
+      )
+    })
+});
+
+// 刪除餐廳
+app.delete("/:restaurant_id", (req, res) => {
+  const { restaurant_id } = req.params
+  Restaurant.findByIdAndDelete(restaurant_id)
+    .then(() => res.redirect("/"))
+    .catch(error => {
+      console.log(error)
+      res.render(
+        'errorPage',
+        { error: error.message }
+      )
+    })
+})
 
 app.listen(port, () => {
   console.log(`Express is listening on localhost:${port}`)
