c8cf0224091abf746fd4992a8296b8264ce649bb
coldwater121112 <coldwater121112@gmail.com>
2023-07-26 19:15:20 +0800

    modify restaurantSeeder

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,24 +1,56 @@
-const Restaurant = require("../restaurant")
-const { db, mongoose } = require('../../config/mongoose')
-
-const restaurantList = require("../../restaurant.json").results
-
+const bcrypt = require('bcryptjs')
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
+const Restaurant = require("../restaurant")
+const User = require('../user')
+const { db, mongoose } = require('../../config/mongoose')
 
+const restaurantList = require("./restaurant.json").results
+
+const SEED_USER = [
+  {
+    name: 'USER1',
+    email: 'user1@example.com',
+    password: '12345678'
+  },
+  {
+    name: 'USER2',
+    email: 'user2@example.com',
+    password: '12345678'
+  }
+]
 
 
 db.on("error", () => {
   console.log("mongodb error!")
 })
 
-db.once("open", () => {
-  console.log("running restaurantSeeder script...")
-  Restaurant.create(restaurantList)
-    .then(() => {
-      console.log("restaurantSeeder done!")
-      db.close()
-    })
-    .catch(err => console.log(err))
+db.once('open', () => {
+    
+  Promise.all(Array.from(SEED_USER, (SEEDUSER, index) => {
+    // console.log(SEEDUSER, index)   
+    return bcrypt
+      .genSalt(10)
+      .then(salt => bcrypt.hash(SEEDUSER.password, salt))
+      .then(hash => User.create({
+        name: SEEDUSER.name,
+        email: SEEDUSER.email,
+        password: hash
+      }))
+      .then(user => {
+        const userId = user._id
+        return Promise.all(Array.from(
+          { length: 3 },
+          (value, i) => {
+            // console.log(i, index, userId)
+            Restaurant.create({ ...restaurantList[index * 3 + i], userId })
+          }
+        ))
+      })
+  }))
+  .then(() => {
+    console.log('done.')
+    process.exit()
+  })
 })
